93765fc9366af5b9830043f6e7dd2a8070e6a28a
ciyinhuang <ciyinhuang@gmail.com>
2016-03-17 19:43:25 +0800

    update

---
+++ src/modules/todo/add/reducers/index.js
@@ -0,0 +1,68 @@
+/**
+ * # authReducer.js
+ *
+ * The reducer for all the actions from the various log states
+ */
+'use strict'
+/**
+ * ## Imports
+ * The InitialState for auth
+ * fieldValidation for validating the fields
+ * formValidation for setting the form's valid flag
+ */
+const InitialState = require('../initialState').default
+
+const {
+
+  "TODO_ITEM",
+  "TODO_ITEM_INIT_START",
+
+  "TODO_ITEM_EXISTS_REQUEST_START",
+  "TODO_ITEM_EXISTS_REQUEST_SUCCESS",
+  "TODO_ITEM_EXISTS_REQUEST_FAILURE",
+
+  "TODO_ITEM_FINDBYID_REQUEST_START",
+  "TODO_ITEM_FINDBYID_REQUEST_SUCCESS",
+  "TODO_ITEM_FINDBYID_REQUEST_FAILURE",
+
+  "TODO_ITEM_FINDONE_REQUEST_START",
+  "TODO_ITEM_FINDONE_REQUEST_SUCCESS",
+  "TODO_ITEM_FINDONE_REQUEST_FAILURE",
+
+  "TODO_ITEM_DELETE_REQUEST_START",
+  "TODO_ITEM_DELETE_REQUEST_SUCCESS",
+  "TODO_ITEM_DELETE_REQUEST_FAILURE",
+
+  "TODO_ITEM_UPDATE_REQUEST_START",
+  "TODO_ITEM_UPDATE_REQUEST_SUCCESS",
+  "TODO_ITEM_UPDATE_REQUEST_FAILURE",
+
+  "TODO_ITEM_UPDATEATTRIBUTES_REQUEST_START",
+  "TODO_ITEM_UPDATEATTRIBUTES_REQUEST_SUCCESS",
+  "TODO_ITEM_UPDATEATTRIBUTES_REQUEST_FAILURE",
+
+} = require('../constants').default
+
+const initialState = new InitialState
+
+/**
+ * ## reducer function
+ * @param {Object} state - initialState
+ * @param {Object} action - type and payload
+ */
+export default function reducer(state = initialState, action) {
+  if (!(state instanceof InitialState)) {
+    return initialState.mergeDeep(state)
+  }
+
+  switch (action.type) {
+
+    // case TODO_ITEM_DELETE_START:
+     
+
+  }
+  /**
+   * ## Default
+   */
+  return state
+}
